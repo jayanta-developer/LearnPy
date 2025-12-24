04e058a98f1356096b3d4abaac1f56218af39efc
Jayanta dey <jayantadey398@gmail.com>
2025-12-25 00:33:09 +0530

    day two

--- dayOne.py
+++ dayOne.py
@@ -8,9 +8,9 @@ def printName(name):
 
 myArr = [2,3,5,6,8,9,1,5,3,6,7,3,5]
 myObj = [
-        {"name":"a","age":21},
-        {"name":"r","age":41},
-        {"name":"u","age":8}
+        {"name":"arup","age":21},
+        {"name":"rahul","age":41},
+        {"name":"upandar","age":8}
     ]
 
 # def multipal(num):
@@ -29,3 +29,22 @@ res1 = next((x for x in myArr if x == 5), None)
 
 for man in myObj:
     print(man["name"], man["age"])
+
+
+
+def age(num):
+    if num > 18:
+        print("You are good to go!")
+    else:
+        print("You cun't go")    
+# age(22)   
+
+
+total = 0
+for item in myArr:
+    total = total + item
+# print(total)
+
+for val in myObj:
+    print(val["name"], "is", val["age"], "years old")
+
